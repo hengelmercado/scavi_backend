544136b7322dabf532a1afbcbeb30846d83d2d3e
jeffbarrios <desarrollador1@tpdindustrial.co>
2022-04-08 07:09:19 -0500

    impor valid

--- routes/registro.js
+++ routes/registro.js
@@ -3,6 +3,7 @@ const { check } = require('express-validator');
 const { validarCampos } = require('../middlewares/validar-campos');
 const { message } = require('../dictionary/dictionary');
 const { obtenerRegistros, obtenerRegistro, actualizarRegistros, borrarRegistros, crearRegistros } = require('../controllers/registro');
+const { validarJWT } = require('../middlewares');
 
 const router = Router();
 
